79622abb74734975756d0e397c14f3346d56cd60
GentlemanThief <145581380+GentlemanThief@users.noreply.github.com>
2024-02-06 16:09:47 -0500

    Update page.js

--- src/app/Hero/page.js
+++ src/app/Hero/page.js
@@ -7,8 +7,8 @@ export default function Hero() {
         <div class="absolute top-0 left-0 w-full h-full overflow-hidden brightness-75">
           <video
             class="min-w-full min-h-full absolute object-cover"
-            src="StrikeFirstHero.webm"
-            type="video/webm"
+            src="StrikeFirstHero.mp4"
+            type="video/mp4"
             autoPlay
             muted
             loop
