484c43f7855f368c48e94f57175ea84179974146
GentlemanThief <145581380+GentlemanThief@users.noreply.github.com>
2024-02-08 20:12:08 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Team StrikeFirst - Home",
-  description: "We‘re a dedicated eSports team specializing in fighting games. From local showdowns to global tournaments, we travel the world, ready to conquer the virtual arena. Join us in the quest for victory!",
+  description: 
+    "We‘re a dedicated eSports team specializing in fighting games. From local showdowns to global tournaments, we travel the world, ready to conquer the virtual arena. Join us in the quest for victory!",
   icon: ["/favicon.ico"],
   apple: ["/apple-touch-icon.png"],
   shortcut: ["/apple-touch-icon.png"],
