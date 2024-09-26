62027afd248cd6de20ace19c098dd19eff977188
Avery Carey <145581380+GentlemanThief@users.noreply.github.com>
2024-09-26 07:36:12 -0400

    Syntax Fix

--- src/app/Hero/page.js
+++ src/app/Hero/page.js
@@ -2,45 +2,47 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <div class="hero min-h-screen">
-      <div class="hero-overlay">
-        <div class="absolute top-0 left-0 w-full h-full overflow-hidden brightness-75">
+    <div className="hero min-h-screen">
+      <div className="hero-overlay">
+        <div className="absolute top-0 left-0 w-full h-full overflow-hidden brightness-75">
           <video
-            class="min-w-full min-h-full absolute object-cover"
+            className="min-w-full min-h-full absolute object-cover"
             autoPlay
             muted
             loop
-            playsInline>
-            <source src="StrikeFirstHero.webm" type="video/webm"></source>
-            <source src="StrikeFirstHero.mp4" type="video/mp4"></source>
+            playsInline
+            aria-label="Background video"
+          >
+            <source src="StrikeFirstHero.webm" type="video/webm" />
+            <source src="StrikeFirstHero.mp4" type="video/mp4" />
           </video>
         </div>
       </div>
-      <div class="hero-content text-center text-white">
-        <div class="max-w-md">
-          <h1 class="xs:text-6xl sm:text-7xl md:text-9xl text-6xl font-black text-white flex justify-center">
+      <div className="hero-content text-center text-white">
+        <div className="max-w-md">
+          <h1 className="xs:text-6xl sm:text-7xl md:text-9xl text-6xl font-black text-white flex justify-center">
             StrikeFirst
           </h1>
-          <a class="text-white text-xs md:text-sm">
+          <p className="text-white text-xs md:text-sm">
             We&lsquo;re a dedicated eSports team specializing in fighting games.
             From local showdowns to global tournaments, we travel the world,
             ready to conquer the virtual arena. Join us in the quest for
             victory!
-          </a>
-          <div class="flex justify-center absolute inset-x-0 bottom-0 h-16">
-            <a href="#about us">
+          </p>
+          <div className="flex justify-center absolute inset-x-0 bottom-0 h-16">
+            <a href="#about-us">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 height="58"
                 width="58"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
                 />
               </svg>
