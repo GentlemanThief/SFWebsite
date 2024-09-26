f258efdbd9e696edc5442d361b41859e219f1dc2
Avery Carey <145581380+GentlemanThief@users.noreply.github.com>
2024-09-26 07:39:40 -0400

    Syntax Fix

--- src/app/AboutUsCarousel/page.js
+++ src/app/AboutUsCarousel/page.js
@@ -8,33 +8,33 @@ import Carousel4 from "public/AboutUsImages/Carousel3.jpg";
 export default function AboutUsCarousel() {
   return (
     <div>
-      <div class="carousel w-full">
-        <div id="slide1" class="carousel-item relative w-full">
-          <Image src={Carousel1} class="object-cover border" alt="GroupPhoto1"/>
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" class="btn btn-circle bg-base-100 hover:bg-base-200">❮</a>
-            <a href="#slide2" class="btn btn-circle bg-base-100 hover:bg-base-200">❯</a>
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <Image src={Carousel1} className="object-cover border" alt="GroupPhoto1"/>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle bg-base-100 hover:bg-base-200">❮</a>
+            <a href="#slide2" className="btn btn-circle bg-base-100 hover:bg-base-200">❯</a>
           </div>
         </div>
-        <div id="slide2" class="carousel-item relative w-full">
-          <Image src={Carousel2} class="object-cover border" alt="GroupPhoto2" />
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" class="btn btn-circle bg-base-100 hover:bg-base-200">❮</a>
-            <a href="#slide3" class="btn btn-circle bg-base-100 hover:bg-base-200">❯</a>
+        <div id="slide2" className="carousel-item relative w-full">
+          <Image src={Carousel2} className="object-cover border" alt="GroupPhoto2" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle bg-base-100 hover:bg-base-200">❮</a>
+            <a href="#slide3" className="btn btn-circle bg-base-100 hover:bg-base-200">❯</a>
           </div>
         </div>
-        <div id="slide3" class="carousel-item relative w-full">
-          <Image src={Carousel3} class="-object-cover border" alt="GroupPhoto3" />
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" class="btn btn-circle bg-base-100 hover:bg-base-200">❮</a>
-            <a href="#slide4" class="btn btn-circle bg-base-100 hover:bg-base-200">❯</a>
+        <div id="slide3" className="carousel-item relative w-full">
+          <Image src={Carousel3} className="object-cover border" alt="GroupPhoto3" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle bg-base-100 hover:bg-base-200">❮</a>
+            <a href="#slide4" className="btn btn-circle bg-base-100 hover:bg-base-200">❯</a>
           </div>
         </div>
-        <div id="slide4" class="carousel-item relative w-full brightness-90">
-          <Image src={Carousel4} class="object-cover border" alt="GroupPhoto4" />
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" class="btn btn-circle bg-base-100 hover:bg-base-200">❮</a>
-            <a href="#slide1" class="btn btn-circle bg-base-100 hover:bg-base-200">❯</a>
+        <div id="slide4" className="carousel-item relative w-full brightness-90">
+          <Image src={Carousel4} className="object-cover border" alt="GroupPhoto4" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle bg-base-100 hover:bg-base-200">❮</a>
+            <a href="#slide1" className="btn btn-circle bg-base-100 hover:bg-base-200">❯</a>
           </div>
         </div>
       </div>
