23c0d0b617ec4c497dab3dc98b7d408caedc6ec2
Avery Carey <145581380+GentlemanThief@users.noreply.github.com>
2024-09-26 04:56:18 -0400

    Syntax Fix

--- src/app/ThemeSwitcher/page.js
+++ src/app/ThemeSwitcher/page.js
@@ -19,15 +19,15 @@ export default function ThemeSwitcher() {
   };
 
   return (
-    <div class="dropdown border">
+    <div className="dropdown border">
       <label tabindex="0">
-        <a class="btn btn-ghost px-4">
+        <a className="btn btn-ghost px-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="17"
             height="17"
             fill="currentColor"
-            class="bi bi-boombox-fill bg-color-white"
+            className="bi bi-boombox-fill bg-color-white"
             viewBox="0 0 16 16"
           >
             <path d="M14 0a.5.5 0 0 1 .5.5V2h.5a1 1 0 0 1 1 1v2H0V3a1 1 0 0 1 1-1h12.5V.5A.5.5 0 0 1 14 0ZM2 3.5a.5.5 0 1 0 1 0 .5.5 0 0 0-1 0Zm2 0a.5.5 0 1 0 1 0 .5.5 0 0 0-1 0Zm7.5.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Zm1.5-.5a.5.5 0 1 0 1 0 .5.5 0 0 0-1 0ZM9.5 3h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1ZM6 10.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm-1.5.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Zm7 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Zm.5-1.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z" />
@@ -37,7 +37,7 @@ export default function ThemeSwitcher() {
       </label>
       <ul
         tabindex="0"
-        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 bg-opacity-0"
+        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 bg-opacity-0"
       >
         <li>
           <button onClick={() => changeTheme("theme-dark")}>Dark Theme</button>
