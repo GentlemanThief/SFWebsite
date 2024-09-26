72369fd65ec29e670b4e7157fdc20b91b0d0c55d
Avery Carey <145581380+GentlemanThief@users.noreply.github.com>
2024-09-26 04:53:31 -0400

    Syntax Fix

--- src/app/TitleHeader/page.js
+++ src/app/TitleHeader/page.js
@@ -1,7 +1,7 @@
 export default function TitleHeader({ title }) {
   return (
     <div>
-      <h1 class="text-5xl font-black text-base-primary p-5 bg-base-100 flex justify-center mx-auto border-y-2">
+      <h1 className="text-5xl font-black text-base-primary p-5 bg-base-100 flex justify-center mx-auto border-y-2">
         {title}
       </h1>
     </div>
