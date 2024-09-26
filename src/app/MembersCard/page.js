458e2043990698fee43912373732f2dfe66bcf8d
Avery Carey <145581380+GentlemanThief@users.noreply.github.com>
2024-09-26 06:31:32 -0400

    Syntax Fix

--- src/app/MembersCard/page.js
+++ src/app/MembersCard/page.js
@@ -6,24 +6,22 @@ export default function MembersCard({
   membersDescription,
 }) {
   return (
-    <div>
-      <div class="bg-base-100">
-        <Image
-          src={membersImage}
-          class="object-cover w-full h-50"
-          alt={membersName}
-          loading="lazy"
-        ></Image>
-        <div class="col-span-1 md:col-span-3">
-          <h2 class="mb-1 text-xl font-bold leading-snug">
-            <a class=" text-primary text-sm px-5 flex justify-center">
-              {membersName}
-            </a>
-          </h2>
-          <p class="text-xs font-normal text-base-content px-5">
-            {membersDescription}
-          </p>
-        </div>
+    <div className="bg-base-100">
+      <Image
+        src={membersImage}
+        className="object-cover w-full h-50"
+        alt={membersName}
+        loading="lazy"
+      />
+      <div className="col-span-1 md:col-span-3">
+        <h2 className="mb-1 text-xl font-bold leading-snug">
+          <a className="text-primary text-sm px-5 flex justify-center">
+            {membersName}
+          </a>
+        </h2>
+        <p className="text-xs font-normal text-base-content px-5">
+          {membersDescription}
+        </p>
       </div>
     </div>
   );
