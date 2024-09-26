b54719d36e548005c0f0594b732f81e465a27ef7
Avery Carey <145581380+GentlemanThief@users.noreply.github.com>
2024-09-26 06:57:24 -0400

    Update page.js

--- src/app/News/page.js
+++ src/app/News/page.js
@@ -9,8 +9,8 @@ export default function News() {
     <div id="news" className="bg-base-200 pt-20">
       <TitleHeader title="News :" />
       <section className="pt-5">
-        <div class="w-full p-4 mx-auto text-center max-w-7xl bg-base-100">
-          <h1 class="text-sm md:text-lg text-base-content max-w-7xl">
+        <div className="w-full p-4 mx-auto text-center max-w-7xl bg-base-100">
+          <h1 className="text-sm md:text-lg text-base-content max-w-7xl">
             &ldquo;Stay updated with the latest in esports and fighting game
             tournaments through Team StrikeFirst&lsquo;s &lsquo;News&lsquo;
             section. Explore game highlights, tournament results, and exclusive
@@ -20,31 +20,31 @@ export default function News() {
       </section>
       <section className="bg-base-200">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 bg-base-200 p-4 mx-auto max-w-8xl">
-          <div class="p-2 bg-base-100 border">
+          <div className="p-2 bg-base-100 border">
             <a href="https://www.youtube.com/watch?v=zumPZvp1Hdk&t">
               <Image
                 src={News1}
-                class="object-cover w-full h-56 mb-1 bg-center border"
+                className="object-cover w-full h-56 mb-1 bg-center border"
                 alt="News1"
                 loading="lazy"
               ></Image>
             </a>
-            <h2 class="text-md font-black text-center">
+            <h2 className="text-md font-black text-center">
               <a
                 href="https://www.youtube.com/watch?v=zumPZvp1Hdk&t"
-                class="text-primary underline"
+                className="text-primary underline"
               >
                 Justin Wong TV FT. DR.B - &ldquo;Beat A Boss Challenge&ldquo;
               </a>
             </h2>
-            <p class="text-sm font-normal text-base-content">
+            <p className="text-sm font-normal text-base-content">
               Check out this video showcasing our team member Galaxy B as they
               take on the exhilarating &ldquo;Beat A Boss Challenge&ldquo;
               alongside the legendary Justin Wong at EVO 2023! Don&lsquo;t miss
               out on the action – watch the video now!
             </p>
           </div>
-          <div class="p-2 bg-base-100 border">
+          <div className="p-2 bg-base-100 border">
             <a href="https://www.youtube.com/watch?v=J7-SwpBReEg&t">
               <Image
                 src={News2}
@@ -61,13 +61,13 @@ export default function News() {
                 Evo Japan 2023 interview FT. Dr.B
               </a>
             </h2>
-            <p class="text-sm font-normal text-base-content">
+            <p className="text-sm font-normal text-base-content">
               Check out this interview as Galaxy B recounts the thrilling
               journey, offering a glimpse into the challenges faced, victories
               achieved, and the overall excitement of their time at EVO Japan.
             </p>
           </div>
-          <div class="p-2 bg-base-100 border">
+          <div className="p-2 bg-base-100 border">
             <a href="https://www.youtube.com/watch?v=hAjRZM6yGMQ">
               <Image
                 src={News3}
@@ -76,15 +76,15 @@ export default function News() {
                 loading="lazy"
               />
             </a>
-            <h2 class="text-md font-black text-center">
+            <h2 className="text-md font-black text-center">
               <a
                 href="https://www.youtube.com/watch?v=hAjRZM6yGMQ"
-                class="text-secondary underline"
+                className="text-secondary underline"
               >
                 StrikeFirst On Button Check Pt. 2 FT. Cookye
               </a>
             </h2>
-            <p class="text-sm font-normal text-base-content">
+            <p className="text-sm font-normal text-base-content">
               Our very own Cookye Fu Sung has been featured on button check.
               Delve into the narrative of Cookye&lsquo;s passion and prowess on
               display at this prestigious event – press play now for an
@@ -93,10 +93,10 @@ export default function News() {
           </div>
         </div>
       </section>
-      <div class="flex justify-center">
+      <div className="flex justify-center">
         <button
           type="button"
-          class="px-6 py-4 text-sm rounded-sm hover:underline bg-base-100 text-base-content"
+          className="px-6 py-4 text-sm rounded-sm hover:underline bg-base-100 text-base-content"
         >
           Load more posts...
         </button>
