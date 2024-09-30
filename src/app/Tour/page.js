d6fd67586c35f13abe009e2e1bc4b2bde846b792
Avery Carey <145581380+GentlemanThief@users.noreply.github.com>
2024-09-30 14:33:37 -0400

    Added FadeIn.

--- src/app/Tour/page.js
+++ src/app/Tour/page.js
@@ -6,6 +6,7 @@ import Tour2 from "public/TourImages/Tour2.png";
 import Tour3 from "public/TourImages/Tour3.png";
 import Tour4 from "public/TourImages/Tour4.png";
 import Tour5 from "public/TourImages/Tour5.png";
+import FadeIn from "../FadeIn/page";
 
 export default function StageSelect() {
   return (
@@ -13,13 +14,15 @@ export default function StageSelect() {
       <TitleHeader title="Tour :" />
       <section className="pt-5">
         <div className="w-full p-4 mx-auto text-center max-w-7xl bg-base-100">
-          <h1 className="text-sm md:text-lg text-base-content max-w-7xl">
-            &ldquo;Embark on a journey with Team StrikeFirst in our
-            &lsquo;Tour&rsquo; section as we travel the globe to compete in
-            thrilling fighting game tournaments. Follow our adventures, relive
-            the electrifying moments, and witness the dedication and skill that
-            make us true contenders in the esports arena.&rdquo;
-          </h1>
+          <FadeIn>
+            <h1 className="text-sm md:text-lg text-base-content max-w-7xl">
+              &ldquo;Embark on a journey with Team StrikeFirst in our
+              &lsquo;Tour&rsquo; section as we travel the globe to compete in
+              thrilling fighting game tournaments. Follow our adventures, relive
+              the electrifying moments, and witness the dedication and skill that
+              make us true contenders in the esports arena.&rdquo;
+            </h1>
+          </FadeIn>
         </div>
       </section>
       <div className="max-w-screen-7xl p-4 mx-auto bg-base-200 text-base-content">
