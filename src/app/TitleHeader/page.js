50a966b85639b28b9251eaf45f8e7c697a5b3e9e
Avery Carey <145581380+GentlemanThief@users.noreply.github.com>
2024-09-30 14:18:36 -0400

    Added FadeIn.

--- src/app/TitleHeader/page.js
+++ src/app/TitleHeader/page.js
@@ -1,8 +1,13 @@
+import dynamic from "next/dynamic";
+const FadeIn = dynamic(() => import("../FadeIn/page"), { ssr: false });
+
 export default function TitleHeader({ title }) {
   return (
     <div>
       <h1 className="text-5xl font-black text-base-primary p-5 bg-base-100 flex justify-center mx-auto border-y-2">
-        {title}
+        <div>
+          <FadeIn>{title}</FadeIn>
+        </div>
       </h1>
     </div>
   );
