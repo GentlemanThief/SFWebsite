23a632e3df3901788f83031c0c68a1b44afb1c00
Avery Carey <145581380+GentlemanThief@users.noreply.github.com>
2024-09-30 14:26:24 -0400

    Added FadeIn.

--- src/app/News/page.js
+++ src/app/News/page.js
@@ -3,6 +3,8 @@ import News1 from "public/NewsImages/News3.png";
 import News2 from "public/NewsImages/News1.png";
 import News3 from "public/NewsImages/News2.png";
 import TitleHeader from "../TitleHeader/page";
+import FadeIn from "../FadeIn/page";
+
 
 export default function News() {
   return (
@@ -10,12 +12,14 @@ export default function News() {
       <TitleHeader title="News :" />
       <section className="pt-5">
         <div className="w-full p-4 mx-auto text-center max-w-7xl bg-base-100">
-          <h1 className="text-sm md:text-lg text-base-content max-w-7xl">
-            &ldquo;Stay updated with the latest in esports and fighting game
-            tournaments through Team StrikeFirst&lsquo;s &lsquo;News&lsquo;
-            section. Explore game highlights, tournament results, and exclusive
-            insights into our journey, all in one place.&ldquo;
-          </h1>
+          <FadeIn>
+            <h1 className="text-sm md:text-lg text-base-content max-w-7xl">
+              &ldquo;Stay updated with the latest in esports and fighting game
+              tournaments through Team StrikeFirst&lsquo;s &lsquo;News&lsquo;
+              section. Explore game highlights, tournament results, and
+              exclusive insights into our journey, all in one place.&ldquo;
+            </h1>
+          </FadeIn>
         </div>
       </section>
       <section className="bg-base-200">
@@ -37,12 +41,14 @@ export default function News() {
                 Justin Wong TV FT. DR.B - &ldquo;Beat A Boss Challenge&ldquo;
               </a>
             </h2>
-            <p className="text-sm font-normal text-base-content">
-              Check out this video showcasing our team member Galaxy B as they
-              take on the exhilarating &ldquo;Beat A Boss Challenge&ldquo;
-              alongside the legendary Justin Wong at EVO 2023! Don&lsquo;t miss
-              out on the action – watch the video now!
-            </p>
+            <FadeIn>
+              <p className="text-sm font-normal text-base-content">
+                Check out this video showcasing our team member Galaxy B as they
+                take on the exhilarating &ldquo;Beat A Boss Challenge&ldquo;
+                alongside the legendary Justin Wong at EVO 2023! Don&lsquo;t miss
+                out on the action – watch the video now!
+              </p>
+            </FadeIn>
           </div>
           <div className="p-2 bg-base-100 border">
             <a href="https://www.youtube.com/watch?v=J7-SwpBReEg&t">
@@ -61,11 +67,13 @@ export default function News() {
                 Evo Japan 2023 interview FT. Dr.B
               </a>
             </h2>
-            <p className="text-sm font-normal text-base-content">
-              Check out this interview as Galaxy B recounts the thrilling
-              journey, offering a glimpse into the challenges faced, victories
-              achieved, and the overall excitement of their time at EVO Japan.
-            </p>
+            <FadeIn>
+              <p className="text-sm font-normal text-base-content">
+                Check out this interview as Galaxy B recounts the thrilling
+                journey, offering a glimpse into the challenges faced, victories
+                achieved, and the overall excitement of their time at EVO Japan.
+              </p>
+            </FadeIn>
           </div>
           <div className="p-2 bg-base-100 border">
             <a href="https://www.youtube.com/watch?v=hAjRZM6yGMQ">
@@ -84,22 +92,26 @@ export default function News() {
                 StrikeFirst On Button Check Pt. 2 FT. Cookye
               </a>
             </h2>
-            <p className="text-sm font-normal text-base-content">
-              Our very own Cookye Fu Sung has been featured on button check.
-              Delve into the narrative of Cookye&lsquo;s passion and prowess on
-              display at this prestigious event – press play now for an
-              exclusive peek into her adventure!
-            </p>
+            <FadeIn>
+              <p className="text-sm font-normal text-base-content">
+                Our very own Cookye Fu Sung has been featured on button check.
+                Delve into the narrative of Cookye&lsquo;s passion and prowess on
+                display at this prestigious event – press play now for an
+                exclusive peek into her adventure!
+              </p>
+            </FadeIn>
           </div>
         </div>
       </section>
       <div className="flex justify-center">
-        <button
-          type="button"
-          className="px-6 py-4 text-sm rounded-sm hover:underline bg-base-100 text-base-content"
-        >
-          Load more posts...
-        </button>
+        <FadeIn>
+          <button
+            type="button"
+            className="px-6 py-4 text-sm rounded-sm hover:underline bg-base-100 text-base-content"
+          >
+            Load more posts...
+          </button>
+        </FadeIn>
       </div>
     </div>
   );
